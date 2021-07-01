5cea985cd34cc028f5c065297c22d0cfe38127fa
zhaodafei <1097625354@qq.com>
2021-07-01 23:33:17 +0800

    no message

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,10 @@
-const less = require("./index.less");
-
-console.log("版本ts");
-
-class Person {
-    private _name: string;
-
-    constructor(name:string) {
-        this._name = name;
-    }
-
-    /*getName() {
-        return this._name;
-    }
-    setName(value: string) {
-        this._name = value;
-    }*/
-
-     // ES6 语法
-    get name(): string {
-        return this._name;
-    }
-    set name(value: string) {
-        this._name = value;
-    }
-
+interface inter2 {
+    length: number;
 }
 
-const per =new Person("daFei");
-// per.setName("论语");
-per.name = "史记"; //  ES6 语法
-console.log(per);
+function fn2<T extends inter2>(name: T): number {
+    return name.length;
+}
+
+fn2<string>("daFei");
+fn2<Array<string>>(["论语", "史记"]);
